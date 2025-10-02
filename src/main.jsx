418e7712ac8b1897863befaf93fd8ec51d48b901
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2025-10-02 14:43:40 +0530

    setup redux store and basic auth slice

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,18 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Toaster } from "react-hot-toast";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+import "./index.css";
+
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
+);
