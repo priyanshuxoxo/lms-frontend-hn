a45d844bd4f38e0f1fe5f82e8e2df7d6c897ee21
priyanshuxoxo <srivastavapriyanshu201@gmail.com>
2025-10-04 16:19:02 +0530

    cleanup about us page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,41 @@
+import apj from "../Assests/images/apj.png";
+import billagtes from "../Assests/images/billGates.png";
+import stevejobs from "../Assests/images/steveJobs.png";
+import nelsonmandela from "../Assests/images/nelsonMandela.png";
+import einstein from "../Assests/images/einstein.png";
+export const celebrities = [
+  {
+    title: "APJ Abdul Kalam",
+    description:
+      "Dream is not that which you see while sleeping, it is something that does not let you sleep.",
+    image: apj,
+    slideNumber: 1,
+  },
+  {
+    title: "Albert Einstein",
+    description:
+      "Try not to become a man of success, but rather try to become a man of value",
+    image: einstein,
+    slideNumber: 2,
+  },
+  {
+    title: "Steve Jobs",
+    description:
+      "Simple can be harder than complex; you have to work hard to get your thinking clean to make it simple.",
+    image: stevejobs,
+    slideNumber: 3,
+  },
+  {
+    title: "Nelson Mandela",
+    description:
+      "Education is the most powerful tool you can use to change the world",
+    image: nelsonmandela,
+    slideNumber: 4,
+  },
+  {
+    title: "Bill Gates",
+    description: "Patience is a key element of success.",
+    image: billagtes,
+    slideNumber: 5,
+  },
+];
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -47,7 +47,7 @@ function HomeLayout({ children }) {
         </div>
         <div className="drawer-side w-0">
           <label className="drawer-overlay" htmlFor="my-drawer"></label>
-          <ul className="menu p-4 w-48 sm:w-80 bg-base-100 text-base-content relative">
+          <ul className="menu p-4 h-[100%] w-48 sm:w-80 bg-base-100 text-base-content relative">
             <li className="w-fit absolute right-2 z-50">
               <button onClick={hideDrawer}>
                 <AiFillCloseCircle size={24} />
@@ -71,7 +71,7 @@ function HomeLayout({ children }) {
               <Link to="/about">About Us </Link>
             </li>
             {!isLoggedIn && (
-              <li className="">
+              <li className="absolute bottom-4 w-[90%]">
                 <div className="w-full flex items-center justify-center gap-2 mt-2">
                   <Link
                     to="/login"
@@ -89,7 +89,7 @@ function HomeLayout({ children }) {
               </li>
             )}
             {isLoggedIn && (
-              <li className="">
+              <li className="absolute bottom-4 w-[90%]">
                 <div className="w-full flex items-center justify-center gap-2 mt-2">
                   <Link
                     to="/user/profile"
